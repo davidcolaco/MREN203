5ed68526e790ce9859d5acc03da34a9d9afedefd
David Colaco <46307251+davidcolaco@users.noreply.github.com>
2025-01-20 12:36:17 -0500

    Add files via upload

---
+++ speedometer.ino
@@ -0,0 +1,149 @@
+/**
+ * @file motor-angular-rate.ino
+ * @author Joshua Marshall (joshua.marshall@queensu.ca)
+ * @brief Arduino program to estimate motor speed from encoder.
+ * @version 2.1
+ * @date 2022-12-09
+ *
+ * @copyright Copyright (c) 2021-2022
+ *
+ */
+
+// Wheel PWM pin (must be a PWM pin)
+int EA = 6;
+
+// Wheel direction digital pins
+int I1 = 7;
+int I2 = 4;
+
+// Motor PWM command variable [0-255]
+byte u = 0;
+
+// Left wheel encoder digital pins
+const byte SIGNAL_A = 2;
+const byte SIGNAL_B = 3;
+const byte SIGNAL_C = 8;
+const byte SIGNAL_D = 9;
+
+// Encoder ticks per (motor) revolution (TPR)
+const int TPR = 3000;
+
+// Wheel radius [m]
+const double RHO = 0.0625;
+
+// Counter to keep track of encoder ticks [integer]
+volatile long encoder_ticksL = 0;
+volatile long encoder_ticksR = 0;
+
+// Variable to store estimated angular rate of left wheel [rad/s]
+double omega_L = 0.0;
+double omega_R = 0.0;
+
+// Sampling interval for measurements in milliseconds
+const int T = 1000;
+
+// Counters for milliseconds during interval
+long t_now = 0;
+long t_last = 0;
+
+// This function is called when SIGNAL_A goes HIGH
+void decodeEncoderTicksL()
+{
+    if (digitalRead(SIGNAL_B) == LOW)
+    {
+        // SIGNAL_A leads SIGNAL_B, so count one way
+        encoder_ticksL--;
+    }
+    else
+    {
+        // SIGNAL_B leads SIGNAL_A, so count the other way
+        encoder_ticksL++;
+    }
+
+}
+void decodeEncoderTicksR()
+{
+
+     if (digitalRead(SIGNAL_D) == LOW)
+    {
+        // SIGNAL_A leads SIGNAL_B, so count one way
+        encoder_ticksR--;
+    }
+    else
+    {
+        // SIGNAL_B leads SIGNAL_A, so count the other way
+        encoder_ticksR++;
+    }
+}
+void setup()
+{
+    // Open the serial port at 9600 bps
+    Serial.begin(9600);
+
+    // Set the pin modes for the motor driver
+    pinMode(EA, OUTPUT);
+    pinMode(I1, OUTPUT);
+    pinMode(I2, OUTPUT);
+
+    // Set the pin modes for the encoders
+    pinMode(SIGNAL_A, INPUT);
+    pinMode(SIGNAL_B, INPUT);
+    pinMode(SIGNAL_C, INPUT);
+    pinMode(SIGNAL_D, INPUT);
+
+    // Every time the pin goes high, this is a pulse
+    attachInterrupt(digitalPinToInterrupt(SIGNAL_A), decodeEncoderTicksL, RISING);
+    attachInterrupt(digitalPinToInterrupt(SIGNAL_C), decodeEncoderTicksR, RISING);
+
+
+    // Print a message
+    Serial.print("Program initialized.");
+    Serial.print("\n");
+}
+
+void loop()
+{
+    // Get the elapsed time [ms]
+    t_now = millis();
+
+    if (t_now - t_last >= T)
+    {
+        // Estimate the rotational speed [rad/s]
+        omega_L = 2.0 * PI * ((double)encoder_ticksL / (double)TPR) * 1000.0 / (double)(t_now - t_last);
+        omega_R = 2.0 * PI * ((double)encoder_ticksR / (double)TPR) * 1000.0 / (double)(t_now - t_last);
+
+        // Print some stuff to the serial monitor
+      
+        Serial.print("Estimated left wheel speed: ");
+        Serial.print(omega_L);
+        Serial.print(" rad/s");
+        Serial.print("    Estimated right wheel speed: ");
+        Serial.print(-omega_R);
+        Serial.print(" rad/s");
+        Serial.print("\n");
+        
+        Serial.print("Translation left wheel speed: ");
+        Serial.print(omega_L*RHO);
+        Serial.print(" m/s");
+        Serial.print("    Translation right wheel speed: ");
+        Serial.print(-omega_R*RHO);
+        Serial.print(" m/s");
+        Serial.print("\n");
+        // Record the current time [ms]
+        t_last = t_now;
+
+        // Reset the encoder ticks counter
+        encoder_ticksL = 0;
+        encoder_ticksR = 0;
+    }
+
+    // Set the wheel motor PWM command [0-255]
+    u = 128;
+
+    // Select a direction
+    digitalWrite(I1, LOW);
+    digitalWrite(I2, HIGH);
+
+    // PWM command to the motor driver
+    analogWrite(EA, u);
+}
